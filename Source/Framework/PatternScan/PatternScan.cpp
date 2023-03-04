ed5dc788721cb2731596934b98d3d190fe8c23d9
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-03-04 16:34:19 +0100

    Split search and match in PatternScan

--- Source/Framework/PatternScan/PatternScan.cpp
+++ Source/Framework/PatternScan/PatternScan.cpp
@@ -8,26 +8,28 @@
 Pattern::Pattern(const char* signature, const char* mask) noexcept {
 	this->signature = signature;
 	this->mask = mask;
+	
+	this->length = strlen(mask);
 }
 
 void* Pattern::searchPattern(void* begin, const void* end) {
 	unsigned int index = 0;
-	unsigned int length = strlen(mask);
 	while (true) {
-		if (end != nullptr && static_cast<char*>(begin) + index + length > end) return nullptr;
-		bool found = true;
-		for (unsigned int i = 0; i < length; i++)
-			if (mask[i] == 'x' &&
-				*(static_cast<unsigned char*>(begin) + index + i) !=
-				static_cast<unsigned char>(signature[i])) {
-				found = false;
-				break;
-			}
-		if (found)
+		if (end != nullptr && static_cast<char*>(begin) + index + length > end)
+			return nullptr;
+		if(matchPattern(static_cast<char*>(begin) + index))
 			break;
 		index++;
 	}
 	return static_cast<char*>(begin) + index;
+}
+
+bool Pattern::matchPattern(void* addr) {
+	for (unsigned int i = 0; i < length; i++)
+		if (mask[i] == 'x' && *(static_cast<unsigned char*>(addr) + i) != static_cast<unsigned char>(signature[i]))
+			return false;
+	
+	return true;
 }
 
 #endif
